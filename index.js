87a404787140413292edcdc69e842adb90f7672c
ajayK108 <ajaykasturi108@gmail.com>
2021-10-18 00:49:57 +0530

    weather app Nodejs

---
+++ index.js
@@ -0,0 +1,41 @@
+
+
+const http = require('http');
+const fs = require('fs');
+var requests = require('requests');
+
+
+const homeFile = fs.readFileSync('home.html', 'utf-8');
+
+const replaceVal = (tempVal, orgVal) =>{
+    let temperature = tempVal.replace("{%tempval%}",orgVal.main.temp);
+    temperature = temperature.replace("{%tempmin%}",orgVal.main.temp_min);
+    temperature = temperature.replace("{%tempmax%}",orgVal.main.temp_max);
+    temperature = temperature.replace("{%location%}",orgVal.name);
+    temperature = temperature.replace("{%country%}",orgVal.sys.country);
+    temperature = temperature.replace("{%tempstatus%}",orgVal.weather[0].main);
+    return temperature;
+};
+
+const server = http.createServer((req, res)=> {
+
+    if(req.url == "/"){
+        requests("http://api.openweathermap.org/data/2.5/weather?q=Solapur&appid=e5d3511f440eeee4058dbb0b26114a09")
+        .on('data', (chunk) => {
+            const objData = JSON.parse(chunk);
+            const arrData = [objData];
+            // console.log(arrData[0].main.temp);
+            const realTimeData = arrData.map((val) => replaceVal(homeFile, val)).join("");
+            res.write(realTimeData);
+            console.log(realTimeData);  
+
+        })
+        .on('end', (err) => {
+            if(err) return console.log("connectoin closed due to errors", err);
+            res.end();
+        });
+    }
+
+});
+
+server.listen(8000, "127.0.0.1");
